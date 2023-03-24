d36cb15079f6064de4290646fa07d13e4dfa2b0f
nasa sig <none@qq.com>
2023-03-24 10:47:01 +0800

    获取多个交易对数据

--- binance-trade-recoed.py
+++ binance-trade-recoed.py
@@ -12,11 +12,6 @@ api_secret = config['keys']['api_secret']
 client = Spot(api_key=api_key, api_secret=api_secret)
 
 
-def get_klines(symbol, interval, start_time, end_time):
-    klines = client.klines(
-        symbol, interval, startTime=start_time, endTime=end_time)
-    return klines
-
 # 获取一天的开始时间和结束时间timestamp格式为毫秒
 def get_day_timestamp(day):
     """
@@ -34,10 +29,10 @@ def get_day_timestamp(day):
 
 # 创建数据表，数据表表名为klines，
 # 字段为open_time, open, high, low, close, volume, close_time, quote_asset_volume, number_of_trades, taker_buy_base_asset_volume, taker_buy_quote_asset_volume，ignore
-def create_table():
+def create_table(symbol="ETHUSDT"):
     conn = sqlite3.connect('binance.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE ethusdt
+    c.execute(f'''CREATE TABLE {symbol}
                  (open_time, open, high, low, close, volume, close_time, quote_asset_volume, number_of_trades, taker_buy_base_asset_volume, taker_buy_quote_asset_volume,ignore)''')
     conn.commit()
     conn.close()
@@ -50,26 +45,43 @@ def get_klines_from_time(symbol, interval, start_time, end_time):
     # 保存数据到数据库
     c = conn.cursor()
     for kline in klines:
-        c.execute("INSERT INTO ethusdt VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", kline)
+        c.execute(f"INSERT INTO {symbol} VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", kline)
     conn.commit()
 
-    if klines[-1][0] < end_time:
+    while klines[-1][0] < end_time:
         # 继续获取下一时刻数据
-        start_time = klines[-1][0] + 60000
+        start_time = klines[-1][0] + 60000 * 5
         print(f"当前获取数据时间为：{start_time}", sep="\r")
-        klines = get_klines_from_time(symbol, interval, start_time, end_time)
-    else:
-        return
+        klines = client.klines(
+            symbol, interval, startTime=start_time, endTime=end_time)
+        c = conn.cursor()
+        for kline in klines:
+            c.execute(
+                f"INSERT INTO {symbol} VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", kline)
+        conn.commit()
+    print("crawl successful")
 
 
 if __name__ == '__main__':
-    create_table()
-    # 2017-9-1
+    # 交易对列表
+    symbol_list = ["ETHUSDT", "BTCUSDT", "LTCUSDT", "BCHUSDT", "EOSUSDT", "XRPUSDT", "ETCUSDT", "ADAUSDT",  "ZECUSDT", "BNBUSDT",
+                   "LINKUSDT",  "USDCUSDT", "MATICUSDT", "FILUSDT","AXSUSDT","DODOUSDT",
+                   "BUSDUSDT",
+                   "DOGEUSDT",
+                   "SUSHIUSDT",
+                   "AVAXUSDT", "DOTUSDT",  "SOLUSDT",  "AAVEUSDT"]
     conn = sqlite3.connect('binance.db')
-    start_time = 1504253880000
-    end_time = 1679500740000  # 2023-3-22 23:59:00
-    symbol = "ETHUSDT"  # 交易对
-    interval = "1m"  # 时间间隔
-    get_klines_from_time(symbol, interval, start_time, end_time)
-    conn.close()
+    for symbol in symbol_list:
+        create_table(symbol)
+        # 2017-9-1
+        start_time = 1504253880000
+        end_time = 1679500740000  # 2023-3-22 23:59:00 1679363700000
+        symbol = "ETHUSDT"  # 交易对
+        interval = "5m"  # 时间间隔
+        try:
+            get_klines_from_time(symbol, interval, start_time, end_time)
+        except Exception as e:
+            print(e)
+            pass
 
+    conn.close()
